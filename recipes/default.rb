d0a51546da768eea9c3db28e925d209d9f7e054f
Aaron Daniel <amdtech@me.com>
2015-03-23 12:59:31 -0700

    Purge postfix and install sendmail

---
+++ recipes/default.rb
@@ -0,0 +1,47 @@
+#
+# Cookbook Name:: sendmail
+# Recipe:: default
+#
+# Copyright 2015, Liftopia, Inc.
+#
+# All rights reserved - Do Not Redistribute
+#
+
+service 'postfix' do
+  action [ :disable, :stop ]
+end
+
+package 'postfix' do
+  action :purge
+end
+
+directory '/etc/postfix' do
+  recursive true
+  action :delete
+end
+
+package 'sendmail'
+
+service 'sendmail' do
+  supports :reload => true, :restart => true
+  action [ :enable, :start ]
+end
+
+execute 'make sendmail config' do
+  command 'make'
+  cwd '/etc/mail'
+  notifies :restart, 'service[sendmail]', :delayed
+  action :nothing
+end
+
+template '/etc/mail/access' do
+  source 'access.erb'
+  mode '0600'
+  notifies :run, 'execute[make sendmail config]', :delayed
+end
+
+template '/etc/mail/sendmail.mc' do
+  source 'sendmail.mc.erb'
+  mode '0644'
+  notifies :run, 'execute[make sendmail config]', :delayed
+end
